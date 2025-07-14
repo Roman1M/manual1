84423ecfc7b7c3aaf020a407a6c657f2c674df29
Roman1M <romanmalko2015@gmail.com>
2025-07-14 14:59:36 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./*.html"],
+  safelist: [
+    'hover:text-cyan-400',
+    'group-hover:text-cyan-400',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
